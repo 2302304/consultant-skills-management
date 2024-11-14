cf2011dbc9bb1bcb2f7bf79cbf97a41e913731f3
Jani Harju <e2302304@edu.vamk.fi>
2024-11-14 12:29:29 +0200

    Etusivun päivitykset ja lisätty alustavasti linkit CV-tulostukseen

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,31 @@
+// src/Pages/Home.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; 
 
 const Home = () => {
-    return <h2>Etusivu</h2>;
+  return (
+    <div className="home-page">
+      {/* Tervetuloteksti */}
+      <h1>Tervetuloa Pinjan osaamisenhallinnan ohjelmistoon!</h1>
+      <p>
+        Tämä ohjelmisto tarjoaa kattavat työkalut konsulttien osaamisen hallintaan.
+        Voit etsiä konsultteja taidoiltaan, tarkastella heidän osaamistaan ja ladata
+        CV:n PDF-muodossa. Sovelluksen avulla voidaan parantaa työn tehokkuutta ja
+        helpottaa oikean osaamisen löytämistä projektiin.
+      </p>
+
+      {/* Linkit CV-tulostukseen */}
+      <div className="cv-links">
+        <Link to="/konsultit" className="cv-link">
+          Hae ja tulosta yksittäisen konsultin CV
+        </Link>
+        <Link to="/tiimi-cv" className="cv-link">
+          Tulosta tiimi-CV mainosmateriaalia varten
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
